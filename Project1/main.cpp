ee3d4fc09dee879b4ad35c30fd3956504533c3de
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-10 13:12:05 +0300

    Refactor code

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -8,6 +8,6 @@ int main() {
 	// This will reserve 12-places, and for empty ones set !
 	std::println("The answer to {} is {:!^12b}", "the ultimate question", answer);
 
-	std::println(answer); // This will give compiler error
+	// std::println(answer); // This will give compiler error
 
 }
