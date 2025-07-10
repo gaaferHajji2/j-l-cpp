5e89cbed38db9a8392b9cbdf910b5bc359ca2eb5
Jafar Loka <gaafer.hajji1995@gmail.com>
2025-07-10 12:45:51 +0300

    Initial commit with .gitignore and C++ 23

---
+++ Project1/main.cpp
@@ -0,0 +1,13 @@
+import std;
+
+int main() {
+	int answer { 42 };
+
+	std::println("The answer is: {}", answer);
+
+	// This will reserve 12-places, and for empty ones set !
+	std::println("The answer to {} is {:!^12b}", "the ultimate question", answer);
+
+	std::println(answer); // This will give compiler error
+
+}
